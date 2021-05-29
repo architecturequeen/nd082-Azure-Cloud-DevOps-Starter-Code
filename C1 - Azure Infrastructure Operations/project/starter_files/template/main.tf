db3907e24d07a581bcab3fb507f8b0dfa37c90ed
architecturequeen <olawunmitaylor@gmail.com>
2021-05-29 20:25:20 +0100

    Update main.tf
    create subnet

--- C1 - Azure Infrastructure Operations/project/starter_files/template/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/template/main.tf
@@ -27,3 +27,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
+
+# Create a subnet in virtual network
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
