358d5c628e73a329c5794d052eb98af52e499cc3
architecturequeen <olawunmitaylor@gmail.com>
2021-05-29 21:49:29 +0100

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/template/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/template/main.tf
@@ -16,21 +16,21 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "resourceGroup"
-  location = "West Europe"
+  name     = "${var.prefix}-rg"
+  location = var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vNet"
+  name                = "${var.prefix}-vnet"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create a subnet in virtual network
 resource "azurerm_subnet" "subnet" {
-  name                 = "internal"
+  name                 = "${var.prefix}-internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.2.0/24"
